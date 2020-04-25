68bdca9844631efe19915dacab7db1745e3c9daf
romanen003 <romanen003@gmail.com>
2020-04-25 23:46:41 +0300

    Initial Commit

---
+++ Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by Роман Гусев on 25.04.2020.
+//  Copyright © 2020 Роман Гусев. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
